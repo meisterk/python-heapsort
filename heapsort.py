12b7cd356b31db57d9430920956c7600eb4a7079
Franz Kohnle <franz@kohnlehome.de>
2020-12-10 15:43:52 +0100

    Add testcases

--- heapsort.py
+++ heapsort.py
@@ -16,10 +16,20 @@ class Heap:
 
 # Testcases
 class TestHeap(unittest.TestCase):
-    def test_vertausche_0_1(self):
+    def test_vertausche_1_2(self):
         heap = Heap([1, 2, 3, 4, 5, 6, 7, 8, 9])
         heap.vertausche(0, 1)
         self.assertEqual(heap.liste, [2, 1, 3, 4, 5, 6, 7, 8, 9])
+
+    def test_vertausche_1_9(self):
+        heap = Heap([1, 2, 3, 4, 5, 6, 7, 8, 9])
+        heap.vertausche(0, 8)
+        self.assertEqual(heap.liste, [9, 2, 3, 4, 5, 6, 7, 8, 1])
+
+    def test_vertausche_1_1(self):
+        heap = Heap([1, 2, 3, 4, 5, 6, 7, 8, 9])
+        heap.vertausche(0, 0)
+        self.assertEqual(heap.liste, [1, 2, 3, 4, 5, 6, 7, 8, 9])
 
 
 # Run Tests
